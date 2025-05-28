af44e7602b171597a3c462e11c66c61c1c85cb2d
JayLeelou <cornellierjeremy@gmail.com>
2025-05-27 22:57:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ async def upload(request: Request):
     body = await request.json()
     proxy = body["proxy"]
     cookies = body["cookies_path"]
-    
+
     result = subprocess.run(
         ["python3", "upload_to_tiktok.py", cookies, proxy],
         capture_output=True,
         text=True
     )
     return {"stdout": result.stdout, "stderr": result.stderr}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)

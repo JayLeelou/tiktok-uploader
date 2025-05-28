ece20101685b8f117ef48efd9111565cc1e4f154
JayLeelou <cornellierjeremy@gmail.com>
2025-05-27 23:13:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 from fastapi import FastAPI, Request
-import subprocess
 
 app = FastAPI()
 
 @app.post("/upload")
 async def upload(request: Request):
-    body = await request.json()
-    proxy = body["proxy"]
-    cookies = body["cookies_path"]
-
-    result = subprocess.run(
-        ["python3", "upload_to_tiktok.py", cookies, proxy],
-        capture_output=True,
-        text=True
-    )
-    return {"stdout": result.stdout, "stderr": result.stderr}
+    return {"message": "✅ FastAPI route is working!"}
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0"
